a91acd95bfc853b180039209afd027116a7ea5fa
Nikolay Makhotkin <nikolay.makhotkin@gmail.com>
2019-12-12 19:05:01 +0400

    Fix output codec

--- test.py
+++ test.py
@@ -216,7 +216,7 @@ def main():
 
     frame_count = vc.get(cv2.CAP_PROP_FRAME_COUNT)
     video_output = args.output
-    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    fourcc = cv2.VideoWriter_fourcc(*'MP4V')
     width = int(vc.get(cv2.CAP_PROP_FRAME_WIDTH))
     height = int(vc.get(cv2.CAP_PROP_FRAME_HEIGHT))
     fps = vc.get(cv2.CAP_PROP_FPS)
